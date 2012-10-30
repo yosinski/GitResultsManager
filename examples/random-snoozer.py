41e9770210eb37e106c27614eda7d3d3d5917325
Jason Yosinski <jason@yosinski.com>
2012-10-30 17:34:19 -0400

    updated sleeper/snoozer programs

--- examples/random-snoozer.py
+++ examples/random-snoozer.py
@@ -5,9 +5,14 @@ from datetime import datetime, timedelta
 import random
 import sys
 
-def sleeper():
-    print 'This program sleeps for random durations, periodically waking up to print the time. Runs until killed.'
+def snoozer():
+    print 'This program sleeps for random durations, periodically waking up to print the time. Runs until reaching its time limit (3 seconds by default).'
     
+    if len(sys.argv) > 1:
+        maxTime = float(sys.argv[1])
+    else:
+        maxTime = 3
+        
     time0 = datetime.now()
     while True:
         sleep(random.uniform(0, .5))
@@ -15,5 +20,7 @@ def sleeper():
         diffSeconds = diff.seconds + diff.microseconds/1000000.0
         print diffSeconds
         sys.stdout.flush()
+        if diffSeconds > maxTime:
+            break
 
-sleeper()
+snoozer()
